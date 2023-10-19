9f0d2504ffeac4b892f3f5b5de3e1ad25e4df6ad
Amir <alternade1@gmail.com>
2023-10-18 23:15:00 -0700

    first commit

---
+++ src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        int[] arrayLength = {1,2,3,4,5};
+        arrayLength[0] = 1; arrayLength[1] = 2;   arrayLength[2] = 3; arrayLength[3] = 4;   arrayLength[4] = 5; 
+        for (int i = 1; i <= 5; i ++ ) {
+            System.out.println(i);
+             
+            
+    }
+    
+    int[] arr_i = {1,2,3,4,5};
+        arr_i[0] = 5; arr_i[1] = 4;   arr_i[2] = 3; arr_i[3] = 2;   arr_i[4] = 1; 
+            for (int y = 5; y > 0; y-- ) {
+            System.out.println(y);
+        } 
+    }
+}

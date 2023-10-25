9e59da1c6da363b6211a1cb348824d81fc753242
Amir <alternade1@gmail.com>
2023-10-25 06:51:55 -0700

    initial commit

--- src/App.java
+++ src/App.java
@@ -1,18 +1,36 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
+        //good job, this is the correct way to make the array
         int[] arrayLength = {1,2,3,4,5};
-        arrayLength[0] = 1; arrayLength[1] = 2;   arrayLength[2] = 3; arrayLength[3] = 4;   arrayLength[4] = 5; 
-        for (int i = 1; i <= 5; i ++ ) {
-            System.out.println(i);
-             
-            
-    }
-    
-    int[] arr_i = {1,2,3,4,5};
-        arr_i[0] = 5; arr_i[1] = 4;   arr_i[2] = 3; arr_i[3] = 2;   arr_i[4] = 1; 
-            for (int y = 5; y > 0; y-- ) {
-            System.out.println(y);
+
+        //right now, arrayLength looks like this: [1,2,3,4,5]
+
+        //this is also correct, but not necessary, since you already made the array
+       
+
+        //right now arrayLength still looks like this: [1,2,3,4,5]
+
+        //this is the correct way to loop something 5 times.  But there is one problem.
+        for (int arr_1 = 1; arr_1 <= 5; arr_1 ++ ) {
+            System.out.println(arr_1);//here, you are printing the value of i, which is 1, then 2, then 3, then 4, then 5
+                                  //however, you are not using the array you made
+                                  //for example, even if the array was [null, null, null, null, null], this would still print 1, 2, 3, 4, 5
+        }
+     int[] arr_2 = {1,2,3,4,5};//good job, this is the correct way to make the array
+
+        //right now, arr_i looks like this: [1,2,3,4,5]
+        //however, remember that arrayLength also looks like this: [1,2,3,4,5]
+        //so, you can just use arrayLength instead of arr_i, since they are identical.
+
+        //this is also correct, but not necessary, since you already made the array
+
+
+        //this is the correct way to loop something 5 times.  But there is the same problem.
+        for (int arr_3 = 5; arr_3 > 0; arr_3-- ) {
+            System.out.println(arr_3);//here, you are printing the value of y, which is 5, then 4, then 3, then 2, then 1
+                                  //however, you are not using the array you made
         } 
+       
     }
 }
